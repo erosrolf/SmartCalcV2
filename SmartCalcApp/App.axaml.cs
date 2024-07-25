44688b95035f1c0f9ac31d185c97583924b65025
erosrolf <erosrolf@gmail.com>
2024-07-25 16:05:01 +0300

    fixed path to AppSettings.json for publish version

--- SmartCalcApp/App.axaml.cs
+++ SmartCalcApp/App.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text.Json;
 using Avalonia;
@@ -7,29 +8,42 @@ using SmartCalcApp.Models;
 using SmartCalcApp.ViewModels;
 using SmartCalcApp.Views;
 
-namespace SmartCalcApp;
-
-public partial class App : Application
+namespace SmartCalcApp
 {
-    public static AppSettings? Settings { get; private set; }
-    
-    public override void Initialize()
+    public partial class App : Application
     {
-        var json = File.ReadAllText("AppSettings.json");
-        Settings = JsonSerializer.Deserialize<AppSettings>(json);
-        AvaloniaXamlLoader.Load(this);
-    }
+        public static AppSettings? Settings { get; private set; }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        public override void Initialize()
         {
-            desktop.MainWindow = new MainWindow
+            string directory = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(directory, "AppSettings.json");
+
+            if (File.Exists(filePath))
             {
-                DataContext = new MainWindowViewModel(),
-            };
+                var json = File.ReadAllText(filePath);
+                Settings = JsonSerializer.Deserialize<AppSettings>(json);
+            }
+            else
+            {
+                Console.WriteLine($"File {filePath} not found!");
+            }
+
+            AvaloniaXamlLoader.Load(this);
         }
-        
-        base.OnFrameworkInitializationCompleted();
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
     }
 }
+ 
